15fb3b04e08b92a90fa9dd9cc06182c617235217
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-11-17 18:52:56 +0200

    Setup for amazon S3 to use EU buckets

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Make Amazon S3 work with bucket in the EU
+AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
+
 # Initialize the Rails application.
 Rails.application.initialize!
