e162425d27139e978122d2a3517cda9fb3609444
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-11-19 01:42:29 +0200

    Remove user input from new projects form

--- spec/requests/projects/new_spec.rb
+++ spec/requests/projects/new_spec.rb
@@ -4,4 +4,8 @@ describe "The new projects page" do
   let(:user) { FactoryGirl.create(:user) }
   let!(:project) { FactoryGirl.create(:project, user: user) }
   let(:new_page) { NewProjectPage.new(project) }
+
+  before { new_page.visit_page_as user }
+
+  it { expect(new_page).not_to have_user_input }
 end
--- spec/support/page_objects/projects/new_project_page.rb
+++ spec/support/page_objects/projects/new_project_page.rb
@@ -5,9 +5,11 @@ class NewProjectPage
   attr_reader :title
 
   def initialize user
-    @user =                 user
-    @page_url =							new_user_project_path(user)
-    @title =								"Create Project"
+    @user = user
+    @page_url = new_user_project_path(user)
+    @title = "Create Project"
+
+    @user_input = "input#project_user_id"
   end
 
   def visit_page
@@ -17,5 +19,10 @@ class NewProjectPage
   def visit_page_as(user)
     user.present? ? login_as(user) : logout
     visit @page_url
+  end
+
+
+  def has_user_input?
+    has_css? @user_input
   end
 end
