4dbf8caa8f270cd2be2b1a469e7b4b85a86448f5
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-12-02 17:50:25 +0200

    Remove http://www from url before_validation

--- app/models/external_account.rb
+++ app/models/external_account.rb
@@ -1,3 +1,12 @@
 class ExternalAccount < ActiveRecord::Base
   belongs_to :user
+
+  before_validation :strip_url!
+
+  def strip_url!
+    self.url = url
+      .gsub("http://", "")
+      .gsub("https://", "")
+      .gsub("www.", "") unless self.url.nil?
+  end
 end
--- spec/models/external_account_spec.rb
+++ spec/models/external_account_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ExternalAccount, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "stripping the url" do
+    [
+      "http://www.google.com",
+      "https://www.google.com",
+      "http://google.com",
+      "https://google.com",
+      "www.google.com",
+      "google.com"
+    ].each do |url|
+      it 'should strip http://www from the url on save' do
+        new_url = ExternalAccount.create(url: url).url
+        expect(new_url).to eq "google.com"
+      end
+    end
+  end
 end
