8393fa8dae68d01de5edce16df45acb584e4bb70
Thomas Morris <thomas.morris@3pillarglobal.com>
2016-01-22 15:02:01 +0200

    Extract interest_list to helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,16 @@ module UsersHelper
     link_to "Projects", user_projects_path(user), class: "show_projects"
   end
 
+
   def edit_profile_link(user)
     link_to "Edit Account", edit_user_registration_path(user), class: "edit_profile"
+  end
+
+
+  # returns comma separated spans each with the passed users interest tags
+  def interest_list user
+    raw user.interest_list.collect { |interest| 
+      content_tag(:span, interest, class: "interest")
+    }.join(", ")
   end
 end
