664b15ebc3a94d3ba1932cf7e51a5f4d73802ff7
Thomas Morris <thomas.morris@3pillarglobal.com>
2016-01-22 13:39:57 +0200

    Add Interest.autocomplete_results and Interest.search

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -2,6 +2,25 @@ class Interest < ActiveRecord::Base
   #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   #%% Class methods
   ##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  def self.autocomplete_results interest_name, exclude
+    search(interest_name, exclude).map do |i|
+      { 
+        label: "#{i.name}  x#{i.taggings_count}", 
+        value: i.name 
+      }
+    end
+  end
+
+
+  # returns all interest tags with passed name, except for those in
+  # names_to_exclude (an array of strings)
+  def self.search name, names_to_exclude
+    ActsAsTaggableOn::Tag
+      .where("(name ILIKE ?)", "%#{name}%")
+      .where.not(name: names_to_exclude)
+  end
+
+
   # changes the input to lowercase then capitalizes the first letter
   def self.capitalize_first input
     return input unless input.instance_of? String
